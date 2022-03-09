533c69492ff2a5b6872fc6f03432abb04a4b6a94
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-03-09 21:08:09 +0500

    Analogue Clock

---
+++ Analogue Clock/script.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+let d = new Date();
+let h = d.getHours();
+let m = d.getMinutes();
+let s = d.getSeconds();
+
+let hRotation = 30*h + m/2;
+let mRotation = 6*m;
+let sRotation = 6 * s;
+
+hours.style.transform = `rotate(${hRotation}deg)`;
+minutes.style.transform = `rotate(${mRotation}deg)`;
+seconds.style.transform = `rotate(${sRotation}deg)`;
+},1000)
