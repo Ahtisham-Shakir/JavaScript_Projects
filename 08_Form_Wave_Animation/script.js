d18f832f490c282c1df29a3a06f4ee5d79a2e004
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-04-01 23:05:21 +0500

    Form Wave Animation javaScript file

--- 08_Form_Wave_Animation/script.js
+++ 08_Form_Wave_Animation/script.js
@@ -2,7 +2,7 @@ const labels = document.querySelectorAll('.form-control label');
 
 labels.forEach(label=>{
     label.innerHTML = label.innerText.split('')
-    .map((letter,idx) => `<span style="transition-delay:${idx*50}ms">${letter}</span>`).join('')
+    .map((letter,idx) => `<span style="transition-delay:${idx*50}ms">${letter}</span>`).join('');
 })
 
 console.log('aa');
